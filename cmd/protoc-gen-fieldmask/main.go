c4fc22eb1c1e2d53384cc3905c9ab897a814f43d
mironov <mironov@group-ib.com>
2022-06-22 18:00:08 +0800

    fix for linter

--- cmd/protoc-gen-fieldmask/main.go
+++ cmd/protoc-gen-fieldmask/main.go
@@ -4,13 +4,14 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/andrey308/protoc-gen-fieldmask/protoc"
 	"golang.org/x/exp/slices"
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/types/pluginpb"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
 const (
@@ -40,7 +41,7 @@ func main() {
 	}.Run(func(plugin *protogen.Plugin) error {
 		plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		if !slices.Contains(supported, strings.ToLower(*lang)) {
-			return errors.New(fmt.Sprintf("unsupported lang: %s, supported languages: go, typescript", *lang))
+			return fmt.Errorf("unsupported lang: %s, supported languages: go, typescript", *lang)
 		}
 		if *maxDepth <= 0 {
 			return errors.New("maxdepth must be bigger than 0")
